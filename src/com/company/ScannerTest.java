a2e18aedc4217beea46cb98b21546920b5b696bf
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-07-08 20:21:26 -0500

    initial commit: just starting this puppy!

---
+++ src/com/company/ScannerTest.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nHello, and welcome to mobile banking! We're happy to meet all of your banking needs.  \nTo get us started, please enter a number: ");
+
+        int a = scanner.nextInt();
+        System.out.println("You typed " + a + "," + "thanks!");
+
+        // write your code here
+    }
+
+}
